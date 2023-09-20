b295e787728cc38dd0d6c4bbec6b601f2e65a545
Jan Czopek <janczopek@hotmail.com>
2023-09-20 11:39:03 +0200

    change to table view

--- src/Content.js
+++ src/Content.js
@@ -1,10 +1,34 @@
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+
 const Content = ({ items }) => {
   return (
-    <ul>
-      {items.map(item => (
-        <li key={item.id}>{JSON.stringify(item)}</li>
-      ))}
-    </ul>
+    <TableContainer component={Paper} sx={{ maxHeight: 440 }}>
+      <Table stickyHeader sx={{ minWidth: 650 }} aria-label="sticky table">
+        <TableHead>
+          <TableRow>
+            {Object.keys(items[0]).map((key) => (
+              <TableCell key={key}>
+                {key}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {items.map((item) => (
+            <TableRow
+              key={item.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              {Object.entries(item).map(([key, value]) => (
+                <TableCell key={key}>
+                  {JSON.stringify(value)}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
 
